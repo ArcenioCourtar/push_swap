a9b7d85bf8e543f8bf45c32e67c61daa08bd9a49
Arcenio Courtar <71286201+ArcenioCourtar@users.noreply.github.com>
2023-02-07 17:25:15 +0100

    Finished the input validation

--- input_validation.c
+++ input_validation.c
@@ -6,35 +6,65 @@
 /*   By: acourtar <acourtar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 16:29:23 by acourtar          #+#    #+#             */
-/*   Updated: 2023/02/07 12:06:02 by acourtar         ###   ########.fr       */
+/*   Updated: 2023/02/07 17:25:04 by acourtar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdlib.h> // exit
 #include <unistd.h> // write
+#include <limits.h> // boundary checking
 
-// static int	are_numbers()
-// {
-// }
-
-// static int	bound_check()
-// {
-// }
-
-// checks for duplicate numbers
-static int	check_duplicates(int argc, char **argv)
+// check if first character is + or -
+static int	isplusmin(char c)
 {
-	int	comparing;
-	int	i;
+	return (c == '+' || c == '-');
+}
+
+// Checks if every argument is a number.
+static int	is_nonum(int argc, char **argv)
+{
+	size_t	len;
+	size_t	i;
 
 	while (argc > 0)
 	{
-		comparing = ft_atoi(argv[argc]);
+		len = ft_strlen(argv[argc]);
+		i = 1;
+		if (isplusmin(argv[argc][0]))
+		{
+			if (len == 1)
+				return (1);
+		}
+		else if (!ft_isdigit(argv[argc][0]))
+			return (1);
+		while (i < len)
+		{
+			if (!ft_isdigit(argv[argc][i]))
+				return (1);
+			i++;
+		}
+		argc--;
+	}
+	return (0);
+}
+
+// checks for duplicate numbers and int over/underflow
+// NOTE: what happens when the long overflows?
+static int	isdupe_isoverflow(int argc, char **argv)
+{
+	long	cmp;
+	int		i;
+
+	while (argc > 0)
+	{
+		cmp = ft_atol(argv[argc]);
+		if (cmp > INT_MAX || cmp < INT_MIN)
+			return (1);
 		i = argc - 1;
 		while (i > 0)
 		{
-			if (comparing == ft_atoi(argv[i]))
+			if (cmp == ft_atol(argv[i]))
 				return (1);
 			i--;
 		}
@@ -48,9 +78,9 @@ void	valid_input(int argc, char **argv)
 {
 	if (argc == 1)
 		exit(EXIT_SUCCESS);
-	if (check_duplicates(argc - 1, argv))
+	if (is_nonum(argc - 1, argv) || isdupe_isoverflow(argc - 1, argv))
 	{
-		write(STDOUT_FILENO, "Error\n", 6);
+		write(STDERR_FILENO, "Error\n", 6);
 		exit(EXIT_SUCCESS);
 	}
 }
