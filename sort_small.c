7903402832fa7ea52035d0132b80f705dfa50596
Arcenio Courtar <71286201+ArcenioCourtar@users.noreply.github.com>
2023-02-21 16:53:32 +0100

    Changed sort_small.c functions
    Instead of tracking the max value in the list, I now track the lowest
    value. Makes the code reusable for the code I made for the big lists!

--- sort_small.c
+++ sort_small.c
@@ -6,7 +6,7 @@
 /*   By: acourtar <acourtar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 14:40:42 by acourtar          #+#    #+#             */
-/*   Updated: 2023/02/21 16:40:59 by acourtar         ###   ########.fr       */
+/*   Updated: 2023/02/21 16:53:24 by acourtar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,24 +46,24 @@ void	sort_three(t_dlist **a)
 	}
 }
 
-static int	correct_loc(t_dlist *a, t_dlist *b, t_dlist *max)
+static int	correct_loc(t_dlist *a, t_dlist *b, t_dlist *min)
 {
 	if (b == NULL && is_sorted(a))
 		return (1);
-	else if (max != NULL)
+	else if (min != NULL)
 	{
-		if (b->num < a->num && a->prev == max)
+		if (a->prev->num < b->num && b->num < a->num)
 			return (1);
-		else if (b->num < a->num && b->num > a->prev->num)
+		else if (a->prev->num < b->num && a == min)
 			return (1);
-		else if (b->num > max->num && a->prev == max)
+		else if (b->num < a->num && a == min)
 			return (1);
 	}
 	return (0);
 }
 
 // bruh
-static void	rotate_calc(t_dlist **a, t_dlist **b, t_dlist *max)
+static void	rotate_calc(t_dlist **a, t_dlist **b, t_dlist *min)
 {
 	t_dlist	*ptr;
 	int		steps;
@@ -72,7 +72,7 @@ static void	rotate_calc(t_dlist **a, t_dlist **b, t_dlist *max)
 	steps = 0;
 	ptr = (*a);
 	len = dlist_count((*a));
-	while (!correct_loc(ptr, (*b), max))
+	while (!correct_loc(ptr, (*b), min))
 	{
 		steps++;
 		ptr = ptr->next;
@@ -97,23 +97,23 @@ static void	rotate_calc(t_dlist **a, t_dlist **b, t_dlist *max)
 
 static void	sort_five_helper(t_dlist **a, t_dlist **b)
 {
-	t_dlist	*max;
+	t_dlist	*min;
 
-	max = (*a)->prev;
+	min = (*a);
 	while ((*b) != NULL)
 	{
-		if ((*b)->num < (*a)->num && (*a)->prev == max)
+		if ((*b)->num < (*a)->num && (*a) == min)
+		{
 			oper_select(a, b, PUSH_A);
+			min = (*a);
+		}
 		else if ((*b)->num < (*a)->num && (*b)->num > (*a)->prev->num)
 			oper_select(a, b, PUSH_A);
-		else if ((*b)->num > max->num && (*a)->prev == max)
-		{
+		else if ((*b)->num > (*a)->prev->num && (*a) == min)
 			oper_select(a, b, PUSH_A);
-			max = (*a);
-		}
 		else
 		{
-			rotate_calc(a, b, max);
+			rotate_calc(a, b, min);
 		}
 	}
 }
